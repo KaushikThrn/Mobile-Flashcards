64aa85e9845203410554986b413f0ccca79e89cc
Kaushik Thiyagarajan <thrn.kaushik@gmail.com>
2018-09-08 15:23:43 +0530

    forms added in deckdetails

--- components/DeckDetails.js
+++ components/DeckDetails.js
@@ -11,15 +11,27 @@ import {
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
 
-
-
 class DeckDetails extends Component {
- 
+    state={question:"",
+           answer:""
+}
+ updateTitle=(key,value)=>{
+        this.setState({[key]:value})
+
+    }
  render(){
       const {navigation}=this.props
         return (
             <View>
                 <Text>{navigation.getParam("deckName")}</Text>
+                <View>
+                    <FormLabel>Question</FormLabel>
+                    <FormInput onChangeText={(text)=>{this.updateTitle("question",text)}} />
+                    <FormLabel>Answer</FormLabel>
+                    <FormInput onChangeText={(text)=>{this.updateTitle("answer",text)}} />
+                    <Text>{this.state.question}</Text>
+                    <Text>{this.state.answer}</Text>
+            </View>
             </View>
         )
     }
